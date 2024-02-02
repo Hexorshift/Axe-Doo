95248860b87d363cf4b22c1f87bba6a7fcba382d
Hexorshift <hexorshift@gmail.com>
2024-02-02 10:12:59 -0500

    New avatar and package updates

--- src/app.js
+++ src/app.js
@@ -13,10 +13,12 @@ const { ActivityType } = require('discord.js');
     await AxeDoo.loadEvents();
     await AxeDoo.loadCommands();
     await AxeDoo.login(process.env.BOT_TOKEN);
-    const avatar = (await AxeDoo.users.fetch('353742902524116992')).avatarURL({
-      size: 512
-    });
-    await AxeDoo.user.setAvatar(avatar);
+    // const avatar = (await AxeDoo.users.fetch('353742902524116992')).avatarURL({
+    //   size: 512
+    // });
+    await AxeDoo.user.setAvatar(
+      'https://cdn.discordapp.com/attachments/821035578240794644/1202677491496390746/bot2.png?ex=65ce53b2&is=65bbdeb2&hm=c78584f367bd4dbfc5876ab607a3e21a7b218d800e6aef1d3816c3fdf5326cbb&'
+    );
     // await AxeDoo.authenticateAI();
 
     AxeDoo.user.setActivity('kuru kuru', { type: ActivityType.Listening });
