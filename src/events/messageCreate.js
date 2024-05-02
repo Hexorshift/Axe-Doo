3dda8b664af8ac2f64644445dbeb9f6299de3ada
Hexorshift <hexorshift@gmail.com>
2024-05-01 22:20:52 -0400

    :/

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,6 +4,7 @@ import wait from '../utils/wait.js';
 import singAllIWantFor from '../utils/singAllIWantFor.js';
 import singAllOfTheOther from '../utils/singAllOfTheOther.js';
 import * as googleTTS from 'google-tts-api';
+import audioTriggers from '../utils/audioTriggers.js';
 import {
   getVoiceConnection,
   createAudioPlayer,
@@ -13,90 +14,46 @@ import {
   AudioPlayerStatus
 } from '@discordjs/voice';
 
-const messageCreate = {
-  name: Events.MessageCreate,
-  once: false,
-  async execute(message) {
-    if (message.author.bot) return;
-    const ttsUsers = ['526449871671001098', '353742902524116992'];
+const ttsGuild = (message) => {
+  if (
+    (message.member.voice.selfMute || message.member.voice.serverMute) &&
+    message.content &&
+    !message.content.match(
+      new RegExp(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
+      )
+    )
+  ) {
+    const voiceChannelInfo = message.member.voice;
+    let connection = getVoiceConnection(voiceChannelInfo.guild.id);
 
-    if (!message.guild) {
-      const member = message.client.guilds.cache.get('760697375949324308').members.cache.get('526449871671001098');
-
-      if (member) {
-        if (
-          message.content &&
-          !message.content.match(
-            new RegExp(
-              /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
-            )
-          )
-        ) {
-          const voiceChannelInfo = member.voice;
-          let connection = getVoiceConnection(voiceChannelInfo.guild.id);
-
-          if (!connection) {
-            connection = joinVoiceChannel({
-              channelId: voiceChannelInfo.channelId,
-              guildId: voiceChannelInfo.guild.id,
-              adapterCreator: voiceChannelInfo.guild.voiceAdapterCreator
-            });
-          }
-
-          const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
-          const results = googleTTS.getAllAudioUrls(message.content, {
-            lang: 'en-US',
-            slow: false,
-            host: 'https://translate.google.com',
-            splitPunct: ',.?'
-          });
-          const resource = createAudioResource(results[0].url);
-
-          player.play(resource);
-          connection.subscribe(player);
-          return;
-        }
-      }
-      return;
-    }
-
-    if (message.content.toLowerCase().includes('yowai mo') && message.member.voice.channelId) {
-      const voiceChannelInfo = message.member.voice;
-      let connection = getVoiceConnection(voiceChannelInfo.guild.id);
-
-      if (!connection) {
-        connection = joinVoiceChannel({
-          channelId: voiceChannelInfo.channelId,
-          guildId: voiceChannelInfo.guild.id,
-          adapterCreator: voiceChannelInfo.guild.voiceAdapterCreator
-        });
-      }
-
-      const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
-      const resource = createAudioResource('https://dl.sndup.net/xr9x/yowai%20mo.mp3');
-
-      player.play(resource);
-      player.on(AudioPlayerStatus.Idle, (oldState, newState) => {
-        player.stop();
-
-        if (
-          !message.guild.channels.cache
-            .get(voiceChannelInfo.channelId)
-            .members.find(({ user }) => ttsUsers.includes(user.id))
-        ) {
-          connection.destroy();
-        }
+    if (!connection) {
+      connection = joinVoiceChannel({
+        channelId: voiceChannelInfo.channelId,
+        guildId: voiceChannelInfo.guild.id,
+        adapterCreator: voiceChannelInfo.guild.voiceAdapterCreator
       });
-      connection.subscribe(player);
-      // console.log(message.member.voice);
-      return;
     }
 
-    // TTS messages
+    const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
+    const results = googleTTS.getAllAudioUrls(message.content, {
+      lang: 'en-US',
+      slow: false,
+      host: 'https://translate.google.com',
+      splitPunct: ',.?'
+    });
+    const resource = createAudioResource(results[0].url);
+
+    player.play(resource);
+    connection.subscribe(player);
+  }
+};
+
+const ttsDM = (message) => {
+  const member = message.client.guilds.cache.get('760697375949324308').members.cache.get('526449871671001098');
+
+  if (member) {
     if (
-      !message.content.includes('yowai mo') &&
-      ttsUsers.includes(message.author.id) &&
-      (message.member.voice.selfMute || message.member.voice.serverMute) &&
       message.content &&
       !message.content.match(
         new RegExp(
@@ -104,7 +61,7 @@ const messageCreate = {
         )
       )
     ) {
-      const voiceChannelInfo = message.member.voice;
+      const voiceChannelInfo = member.voice;
       let connection = getVoiceConnection(voiceChannelInfo.guild.id);
 
       if (!connection) {
@@ -114,18 +71,80 @@ const messageCreate = {
           adapterCreator: voiceChannelInfo.guild.voiceAdapterCreator
         });
       }
-
       const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
+      let resource;
+
+      for (const key in audioTriggers) {
+        if (message.content.toLowerCase().includes(key)) {
+          resource = createAudioResource(audioTriggers[key]);
+
+          player.play(resource);
+          player.on(AudioPlayerStatus.Idle, (oldState, newState) => {
+            player.stop();
+            connection.destroy();
+          });
+          connection.subscribe(player);
+          return;
+        }
+      }
+
       const results = googleTTS.getAllAudioUrls(message.content, {
         lang: 'en-US',
         slow: false,
         host: 'https://translate.google.com',
         splitPunct: ',.?'
       });
-      const resource = createAudioResource(results[0].url);
+      resource = createAudioResource(results[0].url);
 
       player.play(resource);
       connection.subscribe(player);
+    }
+  }
+};
+
+const playAudioTriggersGuild = (message) => {
+  if (message.member.voice.channelId) {
+    for (const key in audioTriggers) {
+      if (message.content.toLowerCase().includes(key)) {
+        const voiceChannelInfo = message.member.voice;
+        let connection = getVoiceConnection(voiceChannelInfo.guild.id);
+
+        if (!connection) {
+          connection = joinVoiceChannel({
+            channelId: voiceChannelInfo.channelId,
+            guildId: voiceChannelInfo.guild.id,
+            adapterCreator: voiceChannelInfo.guild.voiceAdapterCreator
+          });
+        }
+
+        const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
+        const resource = createAudioResource(audioTriggers[key]);
+
+        player.play(resource);
+        player.on(AudioPlayerStatus.Idle, (oldState, newState) => {
+          player.stop();
+          connection.destroy();
+        });
+        connection.subscribe(player);
+      }
+    }
+  }
+};
+
+const messageCreate = {
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const ttsUsers = ['526449871671001098', '353742902524116992'];
+
+    if (ttsUsers.includes(message.author.id)) {
+      if (!message.guild) ttsDM(message);
+      else {
+        ttsGuild(message);
+        playAudioTriggersGuild(message);
+      }
       return;
     }
 
---
+++ src/utils/audioTriggers.js
@@ -0,0 +1,8 @@
+export default {
+  'yowai mo': 'https://dl.sndup.net/xr9x/yowai%20mo.mp3',
+  gyatt: 'https://dl.sndup.net/nvdw/gyatt%20level%20100.mp3',
+  waitwait: 'https://dl.sndup.net/cny6/WAIT%20WAIT.mp3',
+  lethimcookmusic: 'https://dl.sndup.net/kr2y/letcook.mp3',
+  lethimcook: 'https://dl.sndup.net/tsc9/Let%20Him%20Cook%20No%20Music.mp3',
+  dangerknocks: 'https://dl.sndup.net/q8mk/dangerknocks.mp3'
+};
